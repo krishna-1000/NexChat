a1691b6c9cac8a26396c35ebe976f712455eb89d
krishna <krishnpalpatidar17@gmail.com>
2026-03-13 19:39:11 +0530

    project initialization

---
+++ src/test/java/com/nexchat/NexChat/NexChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexchat.NexChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
