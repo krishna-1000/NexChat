fc2b2f1d622b2bf51e5b9edbf0e609b589c094b0
krishna <krishnpalpatidar17@gmail.com>
2026-04-27 16:14:20 +0530

    add new vercel url

--- src/main/java/com/nexchat/NexChat/config/SecurityConfig.java
+++ src/main/java/com/nexchat/NexChat/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://nex-chat-app.vercel.app"
+                "https://nexchat-two.vercel.app"
         ));
         configuration.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
